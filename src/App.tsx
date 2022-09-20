e8c8c29d4904a103abc1029402cfae366cb75724
confidential-nt <youyoy689@gmail.com>
2022-09-20 15:44:41 +0900

    기본 컴포넌트 베이스 구축

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import Login from "./components/login/login";
+import Home from "./components/home/home";
+import Auth from "./service/auth";
+import styles from "./app.module.css";
+import SquareMessage from "./components/message/square-message";
 
-function App() {
+type Props = { auth: Auth };
+
+function App({ auth }: Props) {
+  const [errorPopupState, setErrorPopupState] = useState<string>("");
+
+  const updateErrorPopupState = (errorMessage: string) => {
+    setErrorPopupState(errorMessage);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className={styles.main}>
+      <Routes>
+        <Route path="/home" element={<Home auth={auth} />}></Route>
+        <Route
+          path="/login"
+          element={
+            <Login
+              auth={auth}
+              errorPopupState={errorPopupState}
+              onUpdateErrorPopupState={updateErrorPopupState}
+            />
+          }
+        ></Route>
+        <Route
+          path="/"
+          element={
+            <Login
+              auth={auth}
+              errorPopupState={errorPopupState}
+              onUpdateErrorPopupState={updateErrorPopupState}
+            />
+          }
+        ></Route>
+      </Routes>
+      {errorPopupState && <SquareMessage message={errorPopupState} />}
+    </main>
   );
 }
 
